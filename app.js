674ea2d8c2af1236b9de037208fb7c2efd5fe8d2
lucas150 <97867297+lucas150@users.noreply.github.com>
2024-08-26 19:26:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const express = require("express");
 const app = express();
 const router = require("./routes/router.js");
 const cors = require("cors");
-const port = 8006;
 
-// middle ware
+// Retrieve port from .env or use default
+const port = process.env.PORT || 8006;
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
 app.listen(port, () => {
-  console.log(`server start at port no :${port}`);
+  console.log(`Server started at port no: ${port}`);
 });
