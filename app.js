e704ef41d03b5ea17257cc08010baaf57f6d7fa9
lucas150 <97867297+lucas150@users.noreply.github.com>
2024-08-26 19:42:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
+//require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./routes/router.js");
 const cors = require("cors");
 
 // Retrieve port from .env or use default
-const port = process.env.PORT || 8006;
+const port = 8006;
 
 // Middleware
 app.use(express.json());
