51ee8be7f02376832c9dab86ed3d52b77e34927d
lucas150 <97867297+lucas150@users.noreply.github.com>
2024-08-26 19:56:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const router = require("./routes/router.js");
+const cors = require("cors");
+
+// Retrieve port from .env or use default
+const port = process.env.PORT || 8006;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(router);
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port no: ${port}`);
+});
